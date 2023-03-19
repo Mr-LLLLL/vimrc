26f9d77e0ce8ccc2500926e770ff02f93668466b
zhaogang <zhaogang@dustess.com>
2023-03-19 18:05:03 +0800

    feat:can't display search virtual count

--- lua/core/ui.lua
+++ lua/core/ui.lua
@@ -75,7 +75,7 @@ local function load_noice()
             view_error = "notify",       -- view for errors
             view_warn = "notify",        -- view for warnings
             view_history = "messages",   -- view for :messages
-            view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+            view_search = false, -- view for search count messages. Set to `false` to disable
         },
         popupmenu = {
             enabled = false, -- enables the Noice popupmenu UI
