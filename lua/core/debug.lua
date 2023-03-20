86ecd0490fd8b5837038362ab4c760e9b79ecdde
zhaogang <zhaogang@dustess.com>
2023-03-20 16:34:34 +0800

    perf:restruct

--- lua/core/debug.lua
+++ lua/core/debug.lua
@@ -12,7 +12,7 @@ end
 
 local keys
 
-local dap_stop = function()
+local dap_stop       = function()
     local has_dap, dap = pcall(require, 'dap')
     if has_dap then
         dap.disconnect()
@@ -22,7 +22,7 @@ local dap_stop = function()
     end
 end
 
-local dap_keys = function()
+local dap_keys       = function()
     local keymap_help = {}
     local width = 0
     local line = ''
@@ -47,7 +47,7 @@ local dap_keys = function()
     })
 end
 
-local init_keys = function()
+local init_keys      = function()
     keys = {
             ['R'] = { f = require('dap').run_last, desc = 'restart' },
             ['c'] = { f = require('dap').continue, desc = 'continue' },
@@ -67,10 +67,113 @@ local init_keys = function()
     }
 end
 
+local get_dapvt_conf = function()
+    return {
+        enabled = true,                        -- enable this plugin (the default)
+        enabled_commands = true,               -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+        highlight_changed_variables = true,    -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+        highlight_new_as_changed = false,      -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+        show_stop_reason = true,               -- show stop reason when stopped for exceptions
+        commented = false,                     -- prefix virtual text with comment string
+        only_first_definition = true,          -- only show virtual text at first definition (if there are multiple)
+        all_references = false,                -- show virtual text on all all references of the variable (not only definitions)
+        filter_references_pattern = '<module', -- filter references (not definitions) pattern when all_references is activated (Lua gmatch pattern, default filters out Python modules)
+        -- experimental features:
+        virt_text_pos = 'eol',                 -- position of virtual text, see `:h nvim_buf_set_extmark()`
+        all_frames = false,                    -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+        virt_lines = false,                    -- show virtual lines instead of virtual text (will flicker!)
+        virt_text_win_col = nil                -- position the virtual text at a fixed window column (starting from the first text column) ,
+        -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+    }
+end
+
+local get_dapui_conf = function()
+    return {
+        icons = { expanded = "", collapsed = "", current_frame = "" },
+        mappings = {
+            -- Use a table to apply multiple mappings
+            expand = { "o", "<2-LeftMouse>" },
+            open = "<cr>",
+            remove = "d",
+            edit = "e",
+            -- repl = "r",
+            toggle = "t",
+        },
+        -- Use this to override mappings for specific elements
+        element_mappings = {
+            -- Example:
+            -- stacks = {
+            --   open = "<CR>",
+            --   expand = "o",
+            -- }
+        },
+        -- Expand lines larger than the window
+        -- Requires >= 0.7
+        expand_lines = vim.fn.has("nvim-0.7") == 1,
+        -- Layouts define sections of the screen to place windows.
+        -- The position can be "left", "right", "top" or "bottom".
+        -- The size specifies the height/width depending on position. It can be an Int
+        -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+        -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+        -- Elements are the elements shown in the layout (in order).
+        -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+        layouts = {
+            {
+                elements = {
+                    -- Elements can be strings or table with id and size keys.
+                    { id = "scopes",  size = 0.80 },
+                    { id = "watches", size = 0.20 },
+                    -- { id = "breakpoints", size = 0.15 }
+                },
+                size = 40, -- 40 columns
+                position = "left",
+            },
+            -- {
+            --     elements = {
+            --         "repl",
+            --         -- "stacks",
+            --         -- "console",
+            --     },
+            --     size = 0.25, -- 25% of total lines
+            --     position = "bottom",
+            -- },
+        },
+        controls = {
+            -- Requires Neovim nightly (or 0.8 when released)
+            enabled = true,
+            -- Display controls in this element
+            element = "repl",
+            icons = {
+                pause = "",
+                play = "",
+                step_into = "",
+                step_over = "",
+                step_out = "",
+                step_back = "",
+                run_last = "",
+                terminate = "",
+            },
+        },
+        floating = {
+            max_height = nil,   -- These can be integers or a float between 0 and 1.
+            max_width = nil,    -- Floats will be treated as percentage of your screen.
+            border = "rounded", -- Border style. Can be "single", "double" or "rounded"
+            mappings = {
+                close = { "q", "<Esc>" },
+            },
+        },
+        render = {
+            indent = 1,
+            max_value_lines = 100, -- Can be integer or nil.
+        }
+    }
+end
+
+
 local function load_dap()
     -- this two plug will be rewrite by go.nvim
     local load_dapui = function()
-        require("dapui").setup(require("core.common").get_dapui_conf())
+        require("dapui").setup(get_dapui_conf())
 
         local dap, dapui = require("dap"), require("dapui")
         dap.listeners.after.event_initialized["dapui_config"] = function()
@@ -88,7 +191,7 @@ local function load_dap()
     end
 
     local load_daptvt = function()
-        require("nvim-dap-virtual-text").setup(require("core.common").get_dapvt_conf())
+        require("nvim-dap-virtual-text").setup(get_dapvt_conf())
     end
 
     load_dapui()
