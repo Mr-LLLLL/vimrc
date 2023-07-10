8119f4bef3d1050fdbd966e2ba41ffc05092a32c
zhaogang <zhaogang@dustess.com>
2023-07-10 17:47:39 +0800

    feat:update lspsaga cmd

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -543,7 +543,7 @@ local function load_saga_keymap()
     -- if there is no implement it will hide
     -- when you use action in finder like open vsplit then you can
     -- use <C-t> to jump back
-    km.set("n", "gD", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+    km.set("n", "gD", "<cmd>Lspsaga finder<CR>", { silent = true })
 
     -- Code action
     km.set({ "n", "v" }, "<leader>a", "<cmd>Lspsaga code_action<CR>", { silent = true })
