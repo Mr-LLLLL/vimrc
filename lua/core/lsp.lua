a73ff7585506aa64fcd2d5f40d5da46e5165db36
zhaogang <zhaogang@dustess.com>
2023-07-12 10:34:56 +0800

    feat:update rust and lspsaga config

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -700,6 +700,7 @@ local function load_lspsaga()
         },
     })
     api.nvim_set_hl(0, "SagaBorder", { link = 'CustomBorder' })
+    api.nvim_set_hl(0, "SagaVirtLine", { link = 'CustomVirtualText' })
 
     load_saga_keymap()
 end
@@ -852,6 +853,47 @@ local function load_rust()
     local lspconfig = require("lspconfig")
 
     rt.setup({
+        tools = {
+            -- how to execute terminal commands
+            -- options right now: termopen / quickfix / toggleterm / vimux
+            executor = require("rust-tools.executors").toggleterm,
+            -- These apply to the default RustSetInlayHints command
+            inlay_hints = {
+                -- automatically set inlay hints (type hints)
+                -- default: true
+                auto = true,
+
+                -- Only show inlay hints for the current line
+                only_current_line = false,
+
+                -- whether to show parameter hints with the inlay hints or not
+                -- default: true
+                show_parameter_hints = true,
+
+                -- prefix for parameter hints
+                -- default: "<-"
+                parameter_hints_prefix = "<- ",
+
+                -- prefix for all the other hints (type, chaining)
+                -- default: "=>"
+                other_hints_prefix = "=> ",
+
+                -- whether to align to the length of the longest line in the file
+                max_len_align = false,
+
+                -- padding from the left if max_len_align is true
+                max_len_align_padding = 1,
+
+                -- whether to align to the extreme right or not
+                right_align = false,
+
+                -- padding from the right if right_align is true
+                right_align_padding = 7,
+
+                -- The color of the hints
+                highlight = "CustomVirtualText",
+            },
+        },
         server = {
             on_attach = require("core.common").lsp_on_attack,
             capabilities = require("core.common").lsp_capabilities(),
