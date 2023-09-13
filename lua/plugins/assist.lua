d9a747e941a17dec78893380b5a0987e8ed12957
zhaogang <zhaogang@dustess.com>
2023-09-14 00:03:39 +0800

    test

--- lua/plugins/assist.lua
+++ lua/plugins/assist.lua
@@ -338,10 +338,35 @@ return {
         event = "VeryLazy",
         opts = {
             search = {
-                multi_window = false,
+                multi_window = true,
+            },
+            modes = {
+                search = {
+                    -- when `true`, flash will be activated during regular search by default.
+                    -- You can always toggle when searching with `require("flash").toggle()`
+                    enabled = true,
+                    highlight = { backdrop = false },
+                    jump = { history = true, register = true, nohlsearch = true },
+                    search = {
+                        -- `forward` will be automatically set to the search direction
+                        -- `mode` is always set to `search`
+                        -- `incremental` is set to `true` when `incsearch` is enabled
+                    },
+                },
+                char = {
+                    enabled = true,
+                    highlight = { backdrop = false },
+                    multi_line = false,
+                }
             },
         },
         keys = {
+            {
+                "S",
+                mode = { "n" },
+                function() require("flash").jump({ multi_window = true }) end,
+                desc = "Flash",
+            },
             {
                 "s",
                 mode = { "n", "o", "x" },
