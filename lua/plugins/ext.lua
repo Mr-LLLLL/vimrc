b4f871ab1dfb4fdba27d149a804f333acdd2f69d
zhaogang <zhaogang@dustess.com>
2024-04-06 00:46:58 +0800

    updates

--- lua/plugins/ext.lua
+++ lua/plugins/ext.lua
@@ -869,7 +869,7 @@ return {
 
             daily_notes = {
                 -- Optional, if you keep daily notes in a separate directory.
-                folder = "daily",
+                folder = "notes/dailies",
                 -- Optional, if you want to change the date format for the ID of daily notes.
                 date_format = "%Y-%m-%d",
                 -- Optional, if you want to change the date format of the default alias of daily notes.
