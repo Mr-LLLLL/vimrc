401a10053955b0a7a133448aabcef85d4ac475cb
zhaogang <zhaogang@dustess.com>
2024-05-04 14:31:48 +0800

    removed comment plugins

--- lua/plugins/assist.lua
+++ lua/plugins/assist.lua
@@ -30,72 +30,72 @@ return {
             },
         }
     },
-    {
-        'numToStr/Comment.nvim',
-        keys = {
-            { "gc", nil, mode = { "n", "v" }, desc = "Comment" }
-        },
-        config = function()
-            require('Comment').setup({
-                -- -Add a space b/w comment and the line
-                padding = true,
-                ---Whether the cursor should stay at its position
-                sticky = true,
-                ---Lines to be ignored while (un)comment
-                ignore = nil,
-                ---LHS of toggle mappings in NORMAL mode
-                toggler = {
-                    ---Line-comment toggle keymap
-                    line = 'gcc',
-                    ---Block-comment toggle keymap
-                    block = 'gbc',
-                },
-                ---LHS of operator-pending mappings in NORMAL and VISUAL mode
-                opleader = {
-                    ---Line-comment keymap
-                    line = 'gc',
-                    ---Block-comment keymap
-                    block = 'gb',
-                },
-                ---LHS of extra mappings
-                extra = {
-                    ---Add comment on the line above
-                    above = 'gcO',
-                    ---Add comment on the line below
-                    below = 'gco',
-                    ---Add comment at the end of line
-                    eol = 'gcA',
-                },
-                ---Enable keybindings
-                ---NOTE: If given `false` then the plugin won't create any mappings
-                mappings = {
-                    ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
-                    basic = false,
-                    ---Extra mapping; `gco`, `gcO`, `gcA`
-                    extra = false,
-                },
-                ---Function to call before (un)comment
-                pre_hook = nil,
-                ---Function to call after (un)comment
-                post_hook = nil,
-            })
-
-            -- forbid default mapping, customer my key mapping
-            km.set("n", "gc", '<Plug>(comment_toggle_linewise)', { desc = 'Comment toggle linewise' })
-            km.set("n",
-                "gcc",
-                function()
-                    return api.nvim_get_vvar('count') == 0 and '<Plug>(comment_toggle_linewise_current)'
-                        or '<Plug>(comment_toggle_linewise_count)'
-                end,
-                { expr = true, desc = 'Comment toggle current line' })
-            km.set(
-                "x",
-                "gc",
-                '<Plug>(comment_toggle_linewise_visual)',
-                { desc = 'Comment toggle linewise (visual)' })
-        end
-    },
+    -- {
+    --     'numToStr/Comment.nvim',
+    --     keys = {
+    --         { "gc", nil, mode = { "n", "v" }, desc = "Comment" }
+    --     },
+    --     config = function()
+    --         require('Comment').setup({
+    --             -- -Add a space b/w comment and the line
+    --             padding = true,
+    --             ---Whether the cursor should stay at its position
+    --             sticky = true,
+    --             ---Lines to be ignored while (un)comment
+    --             ignore = nil,
+    --             ---LHS of toggle mappings in NORMAL mode
+    --             toggler = {
+    --                 ---Line-comment toggle keymap
+    --                 line = 'gcc',
+    --                 ---Block-comment toggle keymap
+    --                 block = 'gbc',
+    --             },
+    --             ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+    --             opleader = {
+    --                 ---Line-comment keymap
+    --                 line = 'gc',
+    --                 ---Block-comment keymap
+    --                 block = 'gb',
+    --             },
+    --             ---LHS of extra mappings
+    --             extra = {
+    --                 ---Add comment on the line above
+    --                 above = 'gcO',
+    --                 ---Add comment on the line below
+    --                 below = 'gco',
+    --                 ---Add comment at the end of line
+    --                 eol = 'gcA',
+    --             },
+    --             ---Enable keybindings
+    --             ---NOTE: If given `false` then the plugin won't create any mappings
+    --             mappings = {
+    --                 ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+    --                 basic = false,
+    --                 ---Extra mapping; `gco`, `gcO`, `gcA`
+    --                 extra = false,
+    --             },
+    --             ---Function to call before (un)comment
+    --             pre_hook = nil,
+    --             ---Function to call after (un)comment
+    --             post_hook = nil,
+    --         })
+    --
+    --         -- forbid default mapping, customer my key mapping
+    --         km.set("n", "gc", '<Plug>(comment_toggle_linewise)', { desc = 'Comment toggle linewise' })
+    --         km.set("n",
+    --             "gcc",
+    --             function()
+    --                 return api.nvim_get_vvar('count') == 0 and '<Plug>(comment_toggle_linewise_current)'
+    --                     or '<Plug>(comment_toggle_linewise_count)'
+    --             end,
+    --             { expr = true, desc = 'Comment toggle current line' })
+    --         km.set(
+    --             "x",
+    --             "gc",
+    --             '<Plug>(comment_toggle_linewise_visual)',
+    --             { desc = 'Comment toggle linewise (visual)' })
+    --     end
+    -- },
     {
         "folke/todo-comments.nvim",
         event = "VeryLazy",
