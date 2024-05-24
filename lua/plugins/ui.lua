d7a62ff3bbdcd864cb1774256328165aefc57b4d
zhaogang <zhaogang@dustess.com>
2024-05-24 21:47:07 +0800

    ufo

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -464,10 +464,30 @@ return {
                 return newVirtText
             end
 
+            local function customizeSelector(bufnr)
+                local function handleFallbackException(err, providerName)
+                    if type(err) == 'string' and err:match('UfoFallbackException') then
+                        return require('ufo').getFolds(bufnr, providerName)
+                    else
+                        return require('promise').reject(err)
+                    end
+                end
+
+                return require('ufo').getFolds(bufnr, 'lsp'):catch(function(err)
+                    return handleFallbackException(err, 'treesitter')
+                end):catch(function(err)
+                    return handleFallbackException(err, 'indent')
+                end)
+            end
             require('ufo').setup({
                 fold_virt_text_handler = handler,
                 open_fold_hl_timeout = 150,
-                close_fold_kinds_for_ft = { 'imports', 'comment' },
+                close_fold_kinds_for_ft = {
+                    default = {
+                        'imports',
+                        'comment',
+                    }
+                },
                 preview = {
                     win_config = {
                         border = { '', '─', '', '', '', '─', '', '' },
@@ -482,7 +502,7 @@ return {
                     }
                 },
                 provider_selector = function()
-                    return { "treesitter", "indent" }
+                    return customizeSelector
                 end
             })
 
