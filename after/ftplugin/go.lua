a6fba7138d97ee6a8b21ef2de1969162d6cc54a6
zhaogang <zhaogang@dustess.com>
2024-06-05 11:47:08 +0800

    format go and lua comment

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,7 +1,10 @@
 local bo = vim.bo
+local o = vim.o
 
 do
-    bo.tabstop     = 4
-    bo.shiftwidth  = 4
-    bo.softtabstop = 4
+    bo.tabstop      = 4
+    bo.shiftwidth   = 4
+    bo.softtabstop  = 4
+
+    o.commentstring = "// %s"
 end
