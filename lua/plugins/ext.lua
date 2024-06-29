2ee0365b1b9260c776b8633a757c579f2404ba94
zhaogang <zhaogang@dustess.com>
2024-06-29 13:08:02 +0800

    replace rest.nvim by kulala.nvim

--- lua/plugins/ext.lua
+++ lua/plugins/ext.lua
@@ -279,73 +279,96 @@ return {
         end
     },
     {
-        'rest-nvim/rest.nvim',
-        ft = "http",
-        rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua", "magick" },
-        config = function()
-            require("rest-nvim").setup({
-                client = "curl",
-                env_file = ".env",
-                env_pattern = "\\.env$",
-                env_edit_command = "tabedit",
-                custom_dynamic_variables = {},
-                logs = {
-                    level = "info",
-                    save = true,
-                },
-                -- Skip SSL verification, useful for unknown certificates
-                skip_ssl_verification = true,
-                -- Encode URL before making request
-                encode_url = false,
-                -- Highlight request on run
-                highlight = {
-                    enable = true,
-                    timeout = 750,
-                },
-                result = {
-                    split = {
-                        horizontal = false,
-                        in_place = false,
-                        stay_in_current_window_after_split = true,
-                    },
-                    behavior = {
-                        show_info = {
-                            url = true,
-                            headers = true,
-                            http_info = true,
-                            curl_command = true,
-                        },
-                        -- executables or functions for formatting response body [optional]
-                        -- set them to nil if you want to disable them
-                        formatters = {
-                            json = "jq",
-                            html = function(body)
-                                if vim.fn.executable("tidy") == 0 then
-                                    return body, { found = false, name = "tidy" }
-                                end
-                                local fmt_body = vim.fn.system({
-                                    "tidy",
-                                    "-i",
-                                    "-q",
-                                    "--tidy-mark", "no",
-                                    "--show-body-only", "auto",
-                                    "--show-errors", "0",
-                                    "--show-warnings", "0",
-                                    "-",
-                                }, body):gsub("\n$", "")
-
-                                return fmt_body, { found = true, name = "tidy" }
-                            end,
-                        },
-                    }
-                },
-                keybinds = {
-                    { "<leader>cc", "<cmd>Rest run<cr>",      "Rest Run" },
-                    { "<leader>cp", "<cmd>Rest run last<cr>", "Rest Run Last" },
-                },
+        'mistweaverco/kulala.nvim',
+        keys = {
+            {
+                "<leader>cc",
+                function()
+                    require('kulala').run()
+                end,
+                { noremap = true, silent = true },
+                desc = "Http Request",
+            },
+        },
+        init = function()
+            vim.filetype.add({
+                extension = {
+                    http = "http"
+                }
             })
+        end,
+        config = function()
+            require("kulala").setup()
         end
     },
+    -- {
+    --     'rest-nvim/rest.nvim',
+    --     ft = "http",
+    --     rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua", "magick" },
+    --     config = function()
+    --         require("rest-nvim").setup({
+    --             client = "curl",
+    --             env_file = ".env",
+    --             env_pattern = "\\.env$",
+    --             env_edit_command = "tabedit",
+    --             custom_dynamic_variables = {},
+    --             logs = {
+    --                 level = "info",
+    --                 save = true,
+    --             },
+    --             -- Skip SSL verification, useful for unknown certificates
+    --             skip_ssl_verification = true,
+    --             -- Encode URL before making request
+    --             encode_url = false,
+    --             -- Highlight request on run
+    --             highlight = {
+    --                 enable = true,
+    --                 timeout = 750,
+    --             },
+    --             result = {
+    --                 split = {
+    --                     horizontal = false,
+    --                     in_place = false,
+    --                     stay_in_current_window_after_split = true,
+    --                 },
+    --                 behavior = {
+    --                     show_info = {
+    --                         url = true,
+    --                         headers = true,
+    --                         http_info = true,
+    --                         curl_command = true,
+    --                     },
+    --                     -- executables or functions for formatting response body [optional]
+    --                     -- set them to nil if you want to disable them
+    --                     formatters = {
+    --                         json = "jq",
+    --                         html = function(body)
+    --                             if vim.fn.executable("tidy") == 0 then
+    --                                 return body, { found = false, name = "tidy" }
+    --                             end
+    --                             local fmt_body = vim.fn.system({
+    --                                 "tidy",
+    --                                 "-i",
+    --                                 "-q",
+    --                                 "--tidy-mark", "no",
+    --                                 "--show-body-only", "auto",
+    --                                 "--show-errors", "0",
+    --                                 "--show-warnings", "0",
+    --                                 "-",
+    --                             }, body):gsub("\n$", "")
+    --
+    --                             return fmt_body, { found = true, name = "tidy" }
+    --                         end,
+    --                     },
+    --                 }
+    --             },
+    --             keybinds = {
+    --                 { "<leader>cc", "<cmd>Rest run<cr>",      "Rest Run" },
+    --                 { "<leader>cp", "<cmd>Rest run last<cr>", "Rest Run Last" },
+    --             },
+    --         })
+    --     end
+    -- },
     {
         'kristijanhusak/vim-dadbod-ui',
         cmd = "DBUI",
