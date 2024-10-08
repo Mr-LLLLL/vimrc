a0937517d6ae24940a86b3d3a64033acaa7dddac
zhaogang <zhaogang@dustess.com>
2024-10-08 10:50:02 +0800

    repalce markdown

--- lua/plugins/ext.lua
+++ lua/plugins/ext.lua
@@ -1154,34 +1154,13 @@ return {
         }
     },
     {
-        "OXY2DEV/markview.nvim",
-        -- lazy = false, -- Recommended
-        ft = "markdown", -- If you decide to lazy-load anyway
-
+        'MeanderingProgrammer/render-markdown.nvim',
         dependencies = {
-            -- You will not need this if you installed the
-            -- parsers manually
-            -- Or if the parsers are in your $RUNTIMEPATH
-            "nvim-treesitter/nvim-treesitter",
-
-            "nvim-tree/nvim-web-devicons"
+            'nvim-treesitter/nvim-treesitter',
+            'nvim-tree/nvim-web-devicons',
         },
-        config = function()
-            require("markview").setup({
-                modes = { "n", "no", "c" }, -- Change these modes
-                -- to what you need
-
-                hybrid_modes = { "n" }, -- Uses this feature on
-                -- normal mode
-
-                -- This is nice to have
-                callbacks = {
-                    on_enable = function(_, win)
-                        vim.wo[win].conceallevel = 2;
-                        vim.wo[win].concealcursor = "c";
-                    end
-                }
-            })
-        end
-    },
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    }
 }
