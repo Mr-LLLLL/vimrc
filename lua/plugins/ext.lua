7e3e70a2e0943579eb2ad6ca64821b3cdc812cc0
zhaogang <zhaogang@dustess.com>
2024-12-01 22:32:40 +0800

    update

--- lua/plugins/ext.lua
+++ lua/plugins/ext.lua
@@ -1175,7 +1175,48 @@ return {
         'stevearc/oil.nvim',
         ---@module 'oil'
         ---@type oil.SetupOpts
-        opts = {},
+        opts = {
+            keymaps = {
+                ["g?"] = "actions.show_help",
+                ["<CR>"] = "actions.select",
+                ["<C-v>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+                ["<C-s>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+                ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+                ["<C-p>"] = "actions.preview",
+                ["<C-c>"] = "actions.close",
+                ["<C-l>"] = "actions.refresh",
+                ["-"] = "actions.parent",
+                ["_"] = "actions.open_cwd",
+                ["`"] = "actions.cd",
+                ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory", mode = "n" },
+                ["gs"] = "actions.change_sort",
+                ["gx"] = "actions.open_external",
+                ["g."] = "actions.toggle_hidden",
+                ["g\\"] = "actions.toggle_trash",
+            },
+            float = {
+                -- Padding around the floating window
+                padding = 2,
+                max_width = 0,
+                max_height = 0,
+                border = "rounded",
+                win_options = {
+                    winblend = vim.g.custom_blend,
+                },
+                -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+                get_win_title = nil,
+                -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+                preview_split = "auto",
+                -- This is the config that will be passed to nvim_open_win.
+                -- Change values here to customize the layout
+                override = function(conf)
+                    return conf
+                end,
+            },
+        },
+        keys = {
+            { "-", "<cmd>Oil --float<CR>", { noremap = true, silent = true }, desc = "Oil" },
+        },
         -- Optional dependencies
         -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
         dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
